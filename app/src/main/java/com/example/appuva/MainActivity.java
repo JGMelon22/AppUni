dd263e8b653d25f976a1b7f19a8bc8ab207b3880
JGMelon22 <jgvasconcellos22@gmail.com>
2023-02-24 23:11:20 -0300

    Adicionando mensagem de erro ao detectar usuário offline e tratando possibilidade de retornar a página anterior ao clicar no botão de voltar

--- app/src/main/java/com/example/appuva/MainActivity.java
+++ app/src/main/java/com/example/appuva/MainActivity.java
@@ -1,17 +1,24 @@
 package com.example.appuva;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
@@ -33,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Durante o carregamento da página, a progressbar é vista para dar um feedback de loading
         webView.setWebViewClient(new WebViewClient() {
+
             @Override
             public void onPageStarted(WebView view, String url, Bitmap favicon) {
                 super.onPageStarted(view, url, favicon);
@@ -46,7 +54,25 @@ public class MainActivity extends AppCompatActivity {
                 removeElement(view);
                 progressBar.setVisibility(View.INVISIBLE);
             }
+
+            // Caso o usuário esteja offline, indica que é preciso ter uma conexão de internet ativa
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                webView.loadUrl("about:blank");
+                Toast.makeText(MainActivity.this, "Error de conexão. É necessário estar conectado a internet para usar este aplicativo!", Toast.LENGTH_SHORT).show();
+            }
         });
+    }
+
+    // Caso seja possível retornar a página anterior, o botão de retornar terá essa ação
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
     }
 
     // Remove o link para o Canvas, visto que no navegador mobile fica ruim de usar
