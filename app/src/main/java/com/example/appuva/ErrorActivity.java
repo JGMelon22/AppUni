3b1fdb26236616bfd5ca0b03b840fd6d86cd4beb
JGMelon22 <jgvasconcellos22@gmail.com>
2023-09-21 14:57:58 -0300

    style: removendo ';' desnecessários nos scripts JS

--- app/src/main/java/com/example/appuva/ErrorActivity.java
+++ app/src/main/java/com/example/appuva/ErrorActivity.java
@@ -1,8 +1,14 @@
 package com.example.appuva;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,13 +21,19 @@ public class ErrorActivity extends AppCompatActivity {
     }
 
     public void buttonRetornarOnClick(View view) {
-        try {
-            Intent mainIntent = new Intent(getApplicationContext(), MainActivity.class);
-            startActivity(mainIntent);
-            this.finish();
 
-        } finally {
-            this.finish();
+        // Instancia o network manager e julga se está offline ou não (evitando a web view offline)
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+
+        if (isConnected) {
+            Intent mainIntent = new Intent(ErrorActivity.this, MainActivity.class);
+            startActivity(mainIntent);
+            finish();
+        } else {
+            Toast.makeText(ErrorActivity.this, "Error de conexão. É necessário estar conectado a internet para usar este aplicativo!", Toast.LENGTH_SHORT).show();
         }
     }
+
 }
--- app/src/main/java/com/example/appuva/services/JavaScriptInjectionService.java
+++ app/src/main/java/com/example/appuva/services/JavaScriptInjectionService.java
@@ -8,16 +8,16 @@ public class JavaScriptInjectionService {
 
     public void removeElement(@NonNull WebView webView) {
         // Remove diversidade humana e canvas
-        webView.loadUrl("javascript:(function() {document.querySelector('div.col-sm-1:nth-child(3)').remove();})()");
-        webView.loadUrl("javascript:(function() {document.querySelector('div.col-sm-1:nth-child(2)').remove();})()");
+        webView.loadUrl("javascript:(function() {document.querySelector('div.col-sm-1:nth-child(3)').remove()})()");
+        webView.loadUrl("javascript:(function() {document.querySelector('div.col-sm-1:nth-child(2)').remove()})()");
 
         // Remove botão de reset de senha e de logout
-        webView.loadUrl("javascript:(function() {document.querySelector('[class=\"button-link\"]').remove();})()");
-        webView.loadUrl("javascript:(function() {document.querySelector('[class=\"button-link\"]').remove();})()");
+        webView.loadUrl("javascript:(function() {document.querySelector('[class=\"button-link\"]').remove()})()");
+        webView.loadUrl("javascript:(function() {document.querySelector('[class=\"button-link\"]').remove()})()");
 
         // Remove o link para o Canvas, visto que no navegador mobile fica ruim de usar e uma div em branco
-        webView.loadUrl("javascript:(function() {document.querySelector('[class=\"d-flex justify-content-center\"]').remove();})()");
-        webView.loadUrl("javascript:(function() {document.querySelector('[class=\"container\"]').remove();})()");
+        webView.loadUrl("javascript:(function() {document.querySelector('[class=\"d-flex justify-content-center\"]').remove()})()");
+        webView.loadUrl("javascript:(function() {document.querySelector('[class=\"container\"]').remove()})()");
 
         // Desabilita o clique dos botões na div
         webView.evaluateJavascript("const buttons = document.querySelectorAll('.button-info-center');" +
@@ -28,17 +28,17 @@ public class JavaScriptInjectionService {
 
     // Aplicando um pseudo tema escuro na tela principal do portal do aluno
     public void pseudoDarkTheme(@NonNull WebView webView) {
-        webView.loadUrl("javascript:(function() {document.querySelector('[class=\"container-menu-central\"]').style.backgroundColor = \"#0f3d56\";})()");
-        webView.loadUrl("javascript:(function() {document.body.style.backgroundColor = \"#004b78\";})()");
-        webView.loadUrl("javascript:(function() {document.querySelector('[class=\"modal-body d-flex justify-content-start\"]').style.backgroundColor = \"#8297b5\";})()");
-        webView.loadUrl("javascript:(function() {document.getElementById(\"image-logo\").src=\"/image/Logo-uva-footer.svg\";})()");
-        webView.loadUrl("javascript:(function() {document.getElementById(\"menu-area2\").style.backgroundColor = \"#ebc831\";})()");
-        webView.loadUrl("javascript:(function() {document.getElementById(\"styleMenuFerramentas\").style.backgroundColor = \"#ebc831\";})()");
-        webView.loadUrl("javascript:(function() {document.querySelector('[class=\"buttonCloseBg\"]').style.backgroundColor = \"#ebc831\";})()");
+        webView.loadUrl("javascript:(function() {document.querySelector('[class=\"container-menu-central\"]').style.backgroundColor = \"#0f3d56\"})()");
+        webView.loadUrl("javascript:(function() {document.body.style.backgroundColor = \"#004b78\"})()");
+        webView.loadUrl("javascript:(function() {document.querySelector('[class=\"modal-body d-flex justify-content-start\"]').style.backgroundColor = \"#8297b5\"})()");
+        webView.loadUrl("javascript:(function() {document.getElementById(\"image-logo\").src=\"/image/Logo-uva-footer.svg\"})()");
+        webView.loadUrl("javascript:(function() {document.getElementById(\"menu-area2\").style.backgroundColor = \"#ebc831\"})()");
+        webView.loadUrl("javascript:(function() {document.getElementById(\"styleMenuFerramentas\").style.backgroundColor = \"#ebc831\"})()");
+        webView.loadUrl("javascript:(function() {document.querySelector('[class=\"buttonCloseBg\"]').style.backgroundColor = \"#ebc831\"})()");
 
         // Input de login e senha agora respeitam a cor branca, não mudando para o azul do backround
-        webView.loadUrl("javascript:(function() {document.getElementById(\"LoginEntrada_login\").style.backgroundColor = \"#ffffff\";})()");
-        webView.loadUrl("-javascript:(function() {document.getElementById(\"LoginEntrada_senha\").style.backgroundColor = \"#ffffff\";})()");
+        webView.loadUrl("javascript:(function() {document.getElementById(\"LoginEntrada_login\").style.backgroundColor = \"#ffffff\"})()");
+        webView.loadUrl("-javascript:(function() {document.getElementById(\"LoginEntrada_senha\").style.backgroundColor = \"#ffffff\"})()");
 
         // Input de login e data nascimento na tela de recuperação de senha com fundo branco
         webView.loadUrl("javascript:(function() { " +
@@ -56,29 +56,29 @@ public class JavaScriptInjectionService {
                 "})()");
 
         // Plataforma de cobrança com fundo branco
-        webView.loadUrl("javascript:(function() {document.querySelector('[class=\"container body-content\"]').style.backgroundColor = \"#ffffff\";})()");
+        webView.loadUrl("javascript:(function() {document.querySelector('[class=\"container body-content\"]').style.backgroundColor = \"#ffffff\"})()");
     }
 
     // Estética para mover o banner p/ o canto inferior
     public void beautyTools(@NonNull WebView webView) {
-        webView.loadUrl("javascript:(function() {document.querySelector('[class=\"container-fluid footer-central-page\"]').style.position=\"absolute\";})()");
-        webView.loadUrl("javascript:(function() {document.querySelector('[class=\"container-fluid footer-central-page\"]').style.bottom=\"0\";})()");
-        webView.loadUrl("javascript:(function() {document.querySelector('[class=\"image-footer\"]').style.position=\"absolute\";})()");
-        webView.loadUrl("javascript:(function() {document.querySelector('[class=\"image-footer\"]').style.bottom=\"0\";})()");
+        webView.loadUrl("javascript:(function() {document.querySelector('[class=\"container-fluid footer-central-page\"]').style.position=\"absolute\"})()");
+        webView.loadUrl("javascript:(function() {document.querySelector('[class=\"container-fluid footer-central-page\"]').style.bottom=\"0\"})()");
+        webView.loadUrl("javascript:(function() {document.querySelector('[class=\"image-footer\"]').style.position=\"absolute\"})()");
+        webView.loadUrl("javascript:(function() {document.querySelector('[class=\"image-footer\"]').style.bottom=\"0\"})()");
 
         // Muda a cor da fonte de recuperar senha
-        webView.loadUrl("javascript:(function() {document.querySelector('[class=\"text-muted paragraph-color\"]').style.color = \"#FFFF\";})()");
+        webView.loadUrl("javascript:(function() {document.querySelector('[class=\"text-muted paragraph-color\"]').style.color = \"#FFFF\"})()");
 
         // Muda a logo da tela de login
-        webView.loadUrl("javascript:(function() {document.querySelector('[class=\"header\"]').outerHTML = '<img src=\"https://i.imgur.com/SzxKhmN.jpeg\" alt=\"Image\" style=\"height: auto;\">';})()"); // Funcional
+        webView.loadUrl("javascript:(function() {document.querySelector('[class=\"header\"]').outerHTML = '<img src=\"https://i.imgur.com/SzxKhmN.jpeg\" alt=\"Image\" style=\"height: auto;\">'})()"); // Funcional
 
         // Muda a cor do botão de login e sua fonte
-        webView.loadUrl("javascript:(function() {document.querySelector('[class=\"button-type-mobile btn-style\"]').style.background = \"#ffd000\";})()");
-        webView.loadUrl("javascript:(function() {document.querySelector('[class=\"button-type-mobile btn-style\"]').style.color = \"#014b78\";})()");
+        webView.loadUrl("javascript:(function() {document.querySelector('[class=\"button-type-mobile btn-style\"]').style.background = \"#ffd000\"})()");
+        webView.loadUrl("javascript:(function() {document.querySelector('[class=\"button-type-mobile btn-style\"]').style.color = \"#014b78\"})()");
 
         // Muda a cor do botão de reset de senha e sua fonte
-        webView.loadUrl("javascript:(function() {document.querySelector('[class=\"style-inputs color-btn\"]').style.background = \"#ffd000\";})()");
-        webView.loadUrl("javascript:(function() {document.querySelector('[class=\"style-inputs color-btn\"]').style.color = \"#014b78\";})()");
+        webView.loadUrl("javascript:(function() {document.querySelector('[class=\"style-inputs color-btn\"]').style.background = \"#ffd000\"})()");
+        webView.loadUrl("javascript:(function() {document.querySelector('[class=\"style-inputs color-btn\"]').style.color = \"#014b78\"})()");
     }
 
     public void carterinhaAluno(@NonNull WebView webView) {
