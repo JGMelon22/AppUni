4fbbfe72b1484c1eff51360b2f9d5671e8d7b576
JGMelon22 <jgvasconcellos22@gmail.com>
2023-10-13 10:48:16 -0300

    chore: removendo import desnecessário

--- app/src/main/java/com/example/appuva/MainActivity.java
+++ app/src/main/java/com/example/appuva/MainActivity.java
@@ -16,7 +16,6 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
