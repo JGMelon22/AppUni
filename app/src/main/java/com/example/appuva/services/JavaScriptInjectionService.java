219872d93bde021f315af7fd8dc951272adb1271
JGMelon22 <jgvasconcellos22@gmail.com>
2024-07-27 14:25:49 -0300

    feat: return pesudo dark theme and some visual improvements

--- app/src/main/java/com/example/appuva/services/JavaScriptInjectionService.java
+++ app/src/main/java/com/example/appuva/services/JavaScriptInjectionService.java
@@ -5,6 +5,39 @@ import android.webkit.WebView;
 import androidx.annotation.NonNull;
 
 public class JavaScriptInjectionService {
+
+    public void applyCustomStyle(@NonNull WebView webView) {
+        // Body em azul
+        webView.loadUrl(
+                "javascript:(function() {document.body.style.background = '#004B78'})()"
+        );
+
+        // Muda a logo da tela de login
+        webView.loadUrl(
+                "javascript:(function() {document.querySelector('.header').outerHTML = '<img src=\"https://i.imgur.com/SzxKhmN.jpeg\" alt=\"Image\" style=\"height: auto;\">'})()"
+        );
+
+        // Tag em branco referente ao "Esqueci a senha"
+        webView.loadUrl(
+                "javascript:(function() {document.querySelector('a[href=\"/EsqueciSenhaMobile\"]').style.color = 'White'})()"
+        );
+
+        // Texto Recuperação de senha em branco
+        webView.loadUrl(
+                "javascript:(function() {Array.from(document.querySelectorAll('h1')).find(h1 => h1.textContent.trim() === 'Recuperação de Senha')?.style.setProperty('color', 'white')})()"
+        );
+
+        // Botão de entrar e recuperar em amarelo
+        webView.loadUrl(
+                "javascript:(function() {document.querySelector('button.button-type-mobile.btn-style').style.background = '#FFD000'})()"
+        );
+
+        // Logo superior identica a inferior
+        webView.loadUrl(
+                "javascript:(function() {document.getElementById('image-logo').src='/image/uva/desktop/logo_footer.svg'})()"
+        );
+    }
+
     public void removeElements(@NonNull WebView webView) {
         // Remove diversidade humana
         webView.loadUrl(
